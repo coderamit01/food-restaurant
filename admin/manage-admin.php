6d8aafc9368427b8f7012660555cacc06f153102
amit <amitmohammad46@gmail.com>
2022-09-03 00:09:51 +0600

    add admin sections file

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,6 +4,45 @@
   <section class="main-content">
     <div class="container">
       <h1 class="text-center">Manage Admin</h1>
+      <!-- Add Admin button -->
+      <a class="btn-primary" href="">Add Admin</a>
+      <table class="tbl">
+        <tr>
+          <th>S.N</th>
+          <th>Full Name</th>
+          <th>Username</th>
+          <th>Actions</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Amit Khan</td>
+          <td>wpamit</td>
+          <td>
+            <a class="btn-success" href="">Update Admin</a>
+            <a class="btn-danger" href="">Delete Admin</a>
+          </td>
+        </tr>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Arman Ali</td>
+          <td>dvarman</td>
+          <td>
+            <a class="btn-success" href="">Update Admin</a>
+            <a class="btn-danger" href="">Delete Admin</a>
+          </td>
+        </tr>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Nazmul Hasan</td>
+          <td>nazmul</td>
+          <td>
+            <a class="btn-success" href="">Update Admin</a>
+            <a class="btn-danger" href="">Delete Admin</a>
+          </td>
+        </tr>
+      </table>
     </div>
   </section>
   <!-- Main Content area End here -->
